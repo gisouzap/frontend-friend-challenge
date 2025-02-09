86dd290e0d10531be86ac362e6225d68093a6099
Giselle <gisellesouz.ti@gmail.com>
2025-02-09 12:01:28 -0300

    Add websocket server.js to handle notifications

---
+++ snack-friends/server.js
@@ -0,0 +1,41 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+const users = new Map();
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    const data = JSON.parse(message);
+
+    if (data.type === 'register') {
+      if (!users.has(data.userId)) {
+        users.set(data.userId, new Set());
+      }
+      users.get(data.userId).add(ws);
+    }
+
+    if (
+      data.type === 'sendFriendRequest' ||
+      data.type === 'newFriendAccepted' ||
+      data.type === 'friendRemoved'
+    ) {
+      const receiverConnections = users.get(data.receiverId);
+      if (receiverConnections) {
+        receiverConnections.forEach(connection => {
+          if (connection.readyState === WebSocket.OPEN) {
+            connection.send(JSON.stringify(data));
+          }
+        });
+      }
+    }
+  });
+
+  ws.on('close', () => {
+    users.forEach((connections, userId) => {
+      connections.delete(ws);
+      if (connections.size === 0) {
+        users.delete(userId);
+      }
+    });
+  });
+});
